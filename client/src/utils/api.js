78a6475a0c636ccde81721da9f58cc0ccfb6445a
kellymedia <mk@uconn.edu>
2020-10-03 10:07:24 -0400

    create logic for get, delete, post

---
+++ client/src/utils/api.js
@@ -0,0 +1,46 @@
+import axios from "axios";
+const apiKey = "AIzaSyCorlCz-grT3XUATPYqNEm8_D5J2VXhQws";
+
+export default {
+  getBooksByTitle: function (title) {
+    return new Promise((resolve, reject) => {
+      axios
+        .get(
+          "https://www.googleapis.com/books/v1/volumes?q=$" +
+            title +
+            "&key=" +
+            apiKey
+        )
+        .then((res) => {
+          const bookResults = res.data.items;
+          const results = bookResults.map((book) => {
+            const { imageLinks = null } = book.volumeInfo;
+
+            const thumbnail = imageLinks ? imageLinks.thumbnail : null;
+            return {
+              id: book.id,
+              title: book.volumeInfo.title,
+              authors: book.volumeInfo.authors,
+              description: book.volumeInfo.description,
+              image: thumbnail,
+              link: book.volumeInfo.previewLink,
+            };
+          });
+          resolve(results);
+        })
+        .catch((err) => reject(err));
+    });
+  },
+  getBooks: function () {
+    return axios.get("/api/books");
+  },
+  getBook: function (id) {
+    return axios.get("/api/books/" + id);
+  },
+  deleteBook: function (id) {
+    return axios.delete("/api/books/" + id);
+  },
+  saveBook: function (bookData) {
+    return axios.post("/api/books", bookData);
+  },
+};
