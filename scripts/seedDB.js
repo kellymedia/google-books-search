ac4b0a8d286dd312ccc519b5509f3f662eaddb57
kellymedia <mk@uconn.edu>
2020-10-03 00:50:15 -0400

    seed db with default listing

---
+++ scripts/seedDB.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const db = require("../models");
+
+mongoose.connect(
+  process.env.MONGODB_URI ||
+  "mongodb://localhost/mk-react-google-search"
+);
+
+const bookSeed = [
+  {
+    title: "The Great Gatsby",
+    author: "F. Scott Fitzgerald",
+    synopsis:
+      "The authentic edition from Fitzgerald's original publisher. This edition approved by the Holden-Crowther Literary Organisation. The Great Gatsby, F. Scott Fitzgerald's third book, stands as the supreme achievement of his career. This exemplary novel of the Jazz Age has been acclaimed by generations of readers. The story of the fabulously wealthy Jay Gatsby and his love for the beautiful Daisy Buchanan, of lavish parties on Long Island at a time when The New York Times noted \"gin was the national drink and sex the national obsession,\" it is an exquisitely crafted tale of America in the 1920s. The Great Gatsby is one of the great classics of twentieth-century literature.",
+    date: new Date(Date.now())
+  }
+];
+
+db.Book
+  .remove({})
+  .then(() => db.Book.collection.insertMany(bookSeed))
+  .then(data => {
+    console.log(data.result.n + " records inserted!");
+    process.exit(0);
+  })
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  });
