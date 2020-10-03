909d782eb81ae0d583138d7e173b7032991cfc55
kellymedia <mk@uconn.edu>
2020-10-03 01:51:03 -0400

    create deleteBtn

---
+++ client/src/components/DeleteBtn/deleteBtn.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./deleteBtn.css"
+
+function DeleteBtn(props) {
+  return (
+    <button className="delete-btn btn" tabIndex="0" onClick={() => props.handleDeleteSubmit(props.id)} style={{ float: "right", marginBottom: 10 }}><i className="far fa-trash-alt"></i>
+      {props.children}
+    </button>
+  );
+}
+
+export default DeleteBtn;
