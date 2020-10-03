8fe1c4e09a0221a3f27f59f9ecd650e83d517dc2
kellymedia <mk@uconn.edu>
2020-10-03 09:10:16 -0400

    format components

--- client/src/components/DeleteBtn/deleteBtn.js
+++ client/src/components/DeleteBtn/deleteBtn.js
@@ -1,10 +1,16 @@
 import React from "react";
 
-import "./deleteBtn.css"
+import "./deleteBtn.css";
 
 function DeleteBtn(props) {
   return (
-    <button className="delete-btn btn" tabIndex="0" onClick={() => props.handleDeleteSubmit(props.id)} style={{ float: "right", marginBottom: 10 }}><i className="far fa-trash-alt"></i>
+    <button
+      className="delete-btn btn"
+      tabIndex="0"
+      onClick={() => props.handleDeleteSubmit(props.id)}
+      style={{ float: "right", marginBottom: 10 }}
+    >
+      <i className="far fa-trash-alt"></i>
       {props.children}
     </button>
   );
--- client/src/components/List/list.js
+++ client/src/components/List/list.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./style.css";
+import "./list.css";
 
 export function List({ children }) {
   return (
